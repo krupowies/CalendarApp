ec52b6c3183136a4d689215b96aac2a8d8178ab4
Wojciech Krupowies <krupowieswojciech@gmail.com>
2020-11-02 16:44:36 +0100

    Registration screen, pre-podfile commit

---
+++ CalendarApp/ViewControllers/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  CalendarApp
+//
+//  Created by Wojtek Krupowies on 02/11/2020.
+//  Copyright © 2020 Wojtek Krupowies. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
