02cb89eb40525d6c125b2528bb31f5679c0a2c31
Wojciech Krupowies <krupowieswojciech@gmail.com>
2020-11-04 17:49:59 +0100

    Register new client - done !

--- CalendarApp/ViewControllers/RegisterViewController.swift
+++ CalendarApp/ViewControllers/RegisterViewController.swift
@@ -17,6 +17,8 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var roleSegment: UISegmentedControl!
     
+    let db = Firestore.firestore()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +36,40 @@ class RegisterViewController: UIViewController {
         return nil
     }
     
+    func setUserRole () -> String {
+        if roleSegment.selectedSegmentIndex == 1 {
+            return "coach"
+        } else {return "athlete"}
+    }
+    
     
     @IBAction func registerTapped(_ sender: Any) {
         //Validate textfields. If everything is OK -> crate user. If not -> show error
         let error = validateTextFields()
         if error != nil {
             print(error!)
+        } else {
+            let role = setUserRole()
+            if let username = usernameTextField.text, let email = emailTextField.text, let password = passwordTextField.text {
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    if let e = error {
+                        print(e)
+                    } else {
+                        self.db.collection("users").addDocument(data: [
+                            "username": username,
+                            "email": email,
+                            "ID": result!.user.uid,
+                            "role": role
+                        ]) { (error) in
+                            if let e = error {
+                                print("Problem with saving data to Firestore \(e)")
+                            } else {
+                                 print("Data saved successfully !")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
